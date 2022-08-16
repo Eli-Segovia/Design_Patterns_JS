720de3efef587cc561a6699820a278e913fb938e
Eli <elisego46@gmail.com>
2022-08-16 16:14:22 -0500

    finish code exercise

--- Builder/CodeBuilder.js
+++ Builder/CodeBuilder.js
@@ -13,11 +13,34 @@ class CodeBuilder {
         return this;
     }
 
+    // strigifies and puts into an array to be joined later
     toStringImpl() {
         let classStr = [`class ${this.className} {\n`];
-        for (field of this.fields) {
+        if (this.fields.length > 0) {
+            classStr.push(
+                `${' '.repeat(
+                    CodeBuilder.indentSize()
+                )}constructor(${this.fields.join(', ')}) {\n`
+            );
+            for (const field of this.fields) {
+                classStr.push(
+                    `${' '.repeat(
+                        CodeBuilder.indentSize() * 2
+                    )}this.${field} = ${field};\n`
+                );
+            }
+            classStr.push(`${' '.repeat(CodeBuilder.indentSize())}}\n`);
         }
+
+        classStr.push('}');
+        return classStr.join('');
     }
 
-    toString() {}
+    toString() {
+        return this.toStringImpl();
+    }
 }
+
+let cb = new CodeBuilder('Person');
+
+console.log(cb.toString());
